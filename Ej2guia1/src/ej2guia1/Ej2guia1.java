6b3c4930c2c6e854529ef16775777a7e0d73e8fa
anabela <anabelapolinesi@gmail.com>
2023-08-08 19:50:27 -0300

    mod1
    modificado por anabela

--- Ej2guia1/src/ej2guia1/Ej2guia1.java
+++ Ej2guia1/src/ej2guia1/Ej2guia1.java
@@ -13,8 +13,9 @@ public class Ej2guia1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("soy anabela");
         // TODO code application logic here
     }
+    
     
 }
