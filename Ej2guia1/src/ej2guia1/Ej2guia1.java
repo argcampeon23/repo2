d6e03b5335abf202c2f946fa2d8abb19ef70d165
argcampeon23 <141695617+argcampeon23@users.noreply.github.com>
2023-08-08 19:38:06 -0300

    subo ejercicio de netbeans

---
+++ Ej2guia1/src/ej2guia1/Ej2guia1.java
@@ -0,0 +1,20 @@
+/*
+ 
+ */
+package ej2guia1;
+
+/**
+ *
+ * @author salas
+ */
+public class Ej2guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
